60d88d5d12217641ccc89809f1ea71e7e067f5e9
Shawn <shawntarver7@gmail.com>
2019-02-10 13:42:41 +0100

    recursion finally

--- variable_scope.py
+++ variable_scope.py
@@ -102,3 +102,26 @@ l[1]
      return(f(lang))
 
 callf(greeting)
+
+#Higher order Functions
+
+list = [1,2,3,4]
+for item in map(lambda n: n*2, list): print(item)
+
+list = [1,2,3,4]
+for item in filter(lambda n: n<4, list): print(item)
+
+words=str.split('The longest word in this sentence')
+sorted(words, key=len)
+
+sl=['A','b','a','C','c']
+sl.sort(key=str.lower)
+sl
+sl.sort()
+sl
+
+items=[['rice',2.4,8],["flour",1.9,5],["Corn", 4.7,6]]
+items.sort(key=lambda item: item[1])
+print(items)
+
+#Recursive Functions
