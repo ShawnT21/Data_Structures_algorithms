b770b4e8644492f5a209ea99450574fbe0fbc208
Shawn <shawntarver7@gmail.com>
2019-02-10 14:46:59 +0100

    generators

--- variable_scope.py
+++ variable_scope.py
@@ -124,4 +124,46 @@ items=[['rice',2.4,8],["flour",1.9,5],["Corn", 4.7,6]]
 items.sort(key=lambda item: item[1])
 print(items)
 
+
+
+
 #Recursive Functions
+def iterTest(low,high):
+    while low <= high:
+        print(low)
+        low=low+1
+
+def recurTest(low,high):
+    if low <= high:
+        print(low)
+        recursTest(low+1, high)
+
+#Generators and co-routines
+
+#Compares the running time of a list compared to a Generator
+import time
+#generator function creates an iterator of odd numbers between n and m
+def oddGen(n,m):
+    while n<m:
+      yield n
+      n+=2
+#Builds a list of odd numbers between n and m
+def oddLst(n,m):
+    lst=[]
+    while n<m:
+        lst.append(n)
+        n+=2
+    return lst
+#the time it takes to perform sum on an iterator
+t1=time.time()
+sum(oddGen(1,1000000))
+print("Time to sum an iterator: %f" % (time.time() - t1))
+#the time it takes to build and sum a list
+t1=time.time()
+sum(oddLst(1,1000000))
+print("Time to build and sum a list: %f" % (time.time() - t1))
+
+lst1 =[1,2,3,4]
+gen1 = (10**i for i in lst1)
+gen1
+for x in gen1: print(x)
