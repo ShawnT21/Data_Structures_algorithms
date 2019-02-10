91ef8596376dd62589c36032cc6610276b7838f1
Shawn <shawntarver7@gmail.com>
2019-02-10 12:46:12 +0100

    more lists

--- variable_scope.py
+++ variable_scope.py
@@ -59,3 +59,25 @@ list1 =[x,y,z]
 list2 = list1
 list2[1] = 4
 list1
+
+items = [["rice",2.4, 8], ["flour", 1.9, 5], ["Corn", 4.7, 6] ]
+items[1][1]= items[1][1]*1.2
+for item in items:
+    print("Product: %s Price: %.2f Qaulity: %i" % (item[0], item[1], item[2]))
+
+#List comprehensions
+
+l= [2,4,8,16]
+[i**3 for i in l]
+
+def f1(x): return x*2
+def f2(x): return x*4
+
+lst =[]
+for i in range(16):
+    lst.append(f1(f2(i)))
+print(lst)
+print([f1 (x) for x in range(64) if x in [f2(j) for j in range(16)]])
+
+list1= [[1,2,3], [4,5,6]]
+[i*j for i in list1[0] for j in list1[1]]
