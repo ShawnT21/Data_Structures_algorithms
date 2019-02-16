ea7cfd039b2fd2f3cdecf5473e2735fd02c4d5c8
Shawn <shawntarver7@gmail.com>
2019-02-16 23:15:15 +0100

    data types

---
+++ data_types.py
@@ -0,0 +1,6 @@
+a=4; b= 5 # Operator (=) assigns the value to variable
+print(a, "is of type", type(a))
+9/5
+c = b/a # division returns a floating point number
+print(c, "is of type", type(c))
+c       # No need to explicitly delcare the datatype
