85a52cf1f716d7b47a2a4ca3d31cbaeb65b66eff
Shawn <shawntarver7@gmail.com>
2019-02-16 23:19:02 +0100

    floor operator

--- data_types.py
+++ data_types.py
@@ -4,3 +4,9 @@ print(a, "is of type", type(a))
 c = b/a # division returns a floating point number
 print(c, "is of type", type(c))
 c       # No need to explicitly delcare the datatype
+
+a=4; b=5
+d= b//a
+print(d, "is of type", type(d))
+7/5 # true division
+-7//5 # floor division operator
