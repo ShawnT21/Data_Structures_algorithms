fe08c3cab33f0156936433bf0dce0d931bc687a8
Shawn <shawntarver7@gmail.com>
2019-02-24 18:09:52 +0100

    algorithms are awesome just wait I'll get fustrated soon

--- algorithm _design _principles.py
+++ algorithm _design _principles.py
@@ -34,3 +34,32 @@ def bitStr(n,s):
     return [digit + bits for digit in bitStr(1,s) for bits in bitStr(n-1,s)]
 
 print(bitStr(3,'abc'))
+
+#Karatsuba algorithm
+from math import log10
+def karatsuba(x,y):
+
+    #The base case for recusion
+    if x<10 or y<10:
+        return x*y
+
+    #sets n, the number of digits in the highest input number
+    n=max(int(log10(x)+1), int(log10(y)+1))
+
+    #rounds up n/2
+    n_2 = int(math.ceil(n/2.0))
+    #adds 1 if n is uneven
+    n = n if n%2 == 0 else n+1
+    #splits the input numbers
+    a, b = divmod(x, 10**n_2)
+    c, d = divmod(y, 10**n_2)
+    #applies the three recursive steps
+    ac = karatsuba(a,c)
+    bd = karatsuba(b,d)
+    ad_bc = karatsuba((a+b), (c+d))-ac-bd
+
+     #performs the multiplication
+    return (((10**n)*ac)+bd+((10**n_2)*(ab_bc)))
+
+t= karatsuba(1234,3456)
+print(t)
